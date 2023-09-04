00dcef8e580fd463568477a0a89ed78a3abbe3f0
Jose Mosquera <jojumofu@gmail.com>
2023-09-04 15:52:57 -0500

    fix: typos and dotenv load

--- ecommerce/payment/urls.py
+++ ecommerce/payment/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "payment"
 
-urlspatterns = [
+urlpatterns = [
     path("process/", views.payment_process, name="process"),
     path("completed/", views.payment_completed, name="completed"),
     path("canceled/", views.payment_canceled, name="canceled"),
