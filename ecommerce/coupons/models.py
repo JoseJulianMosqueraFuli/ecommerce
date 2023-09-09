96ff85dde2a08567b0854b06fa02b8e382a4843c
Jose Mosquera <jojumofu@gmail.com>
2023-09-09 13:34:00 -0500

    feat: add coupon model class

--- ecommerce/coupons/models.py
+++ ecommerce/coupons/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(50)],
+        help_text="Percentage value of discount (0 to 100)",
+    )
+
+    def __str__(self) -> str:
+        return self.code
